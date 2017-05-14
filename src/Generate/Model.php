b8b5d9d0984ec37fb6c179a2b2e06fbed1133e68
Pavol Perdík <palypster@gmail.com>
2017-05-14 20:06:02 +0200

    ability to specify model name with namespace

--- src/Generate/Model.php
+++ src/Generate/Model.php
@@ -1,6 +1,7 @@
 <?php namespace Brackets\AdminGenerator\Generate;
 
 use Illuminate\Support\Str;
+use Symfony\Component\Console\Input\InputOption;
 
 class Model extends Generator {
 
@@ -27,9 +28,10 @@ class Model extends Generator {
     {
 
         $tableName = $this->argument('table_name');
-        $modelName = Str::studly(Str::singular($tableName));
-        $modelFullName = $this->qualifyClass("Models\\".$modelName);
+        $modelName = class_basename($this->option('model')) ?: Str::studly(Str::singular($tableName));
+        $modelFullName = $this->qualifyClass("Models\\".($this->option('model') ?: Str::studly(Str::singular($tableName))));
         $modelPath = $path = $this->getPath($modelFullName);
+        $modelNamespace = Str::replaceLast("\\".$modelName, '', $modelFullName);
 
         if ($this->alreadyExists($modelPath)) {
             $this->error('File '.$modelPath.' already exists!');
@@ -38,7 +40,7 @@ class Model extends Generator {
 
         $this->makeDirectory($path);
 
-        $this->files->put($path, $this->buildClass($tableName, $modelName));
+        $this->files->put($path, $this->buildClass($tableName, $modelName, $modelNamespace));
 
         // TODO think if we should use ide-helper:models ?
 
@@ -46,11 +48,11 @@ class Model extends Generator {
 
     }
 
-    protected function buildClass($tableName, $modelName) {
+    protected function buildClass($tableName, $modelName, $modelNamespace) {
 
         return view('brackets/admin-generator::model', [
             'modelName' => $modelName,
-            'namespace' => 'App\Models',
+            'namespace' => $modelNamespace,
             'dates' => $this->readColumnsFromTable($tableName)->filter(function($column) {
                 return $column['type'] == "datetime" || $column['type'] == "date";
             })->pluck('name'),
@@ -58,6 +60,12 @@ class Model extends Generator {
                 return !in_array($column['name'], ['id', 'created_at', 'updated_at']);
             })->pluck('name'),
         ])->render();
+    }
+
+    protected function getOptions() {
+        return [
+            ['model', 'm', InputOption::VALUE_OPTIONAL, 'Specify custom model name (namespaced without App\Models prefix)'],
+        ];
     }
 
 }
