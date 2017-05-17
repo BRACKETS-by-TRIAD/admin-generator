1bcfecfb8c8c7eeb19b6c362c232d274f475e0d6
Pavol Perdík <palypster@gmail.com>
2017-05-17 17:36:48 +0200

    ..

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -22,12 +22,14 @@
         @endforeach
 
         <td>
-            <a class="btn btn-info btn-xs" href="{{'{{'}} route('admin/{{ $objectName }}/edit', ['{{ $objectName }}' => ${{ $objectName }}]) }}" role="button">Edit</a>
-            <form method="post" action="{{'{{'}} route('admin/{{ $objectName }}/destroy', ['{{ $objectName }}' => ${{ $objectName }}]) }}" class="form-inline">
-                <input name="_method" type="hidden" value="DELETE">
-                {{'{'}}!! csrf_field() !!{{'}'}}
-                <button type="submit" class="btn btn-xs btn-danger">Delete</button>
-            </form>
+            <div class="form-inline">
+                <a class="btn btn-info btn-xs" href="{{'{{'}} route('admin/{{ $objectName }}/edit', ['{{ $objectName }}' => ${{ $objectName }}]) }}" role="button">Edit</a>
+                <form class="form-group" action="{{'{{'}} route('admin/{{ $objectName }}/destroy', ['{{ $objectName }}' => ${{ $objectName }}]) }}" method="post">
+                    <input name="_method" type="hidden" value="DELETE">
+                    {{'{'}}!! csrf_field() !!{{'}'}}
+                    <button type="submit" class="btn btn-xs btn-danger">Delete</button>
+                </form>
+            </div>
         </td>
     </tr>
     {{'@'}}endforeach
