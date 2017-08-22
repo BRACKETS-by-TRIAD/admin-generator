a3ef662c52e32d623b298d0482bea6419c3753a6
Pavol Perdík <palypster@gmail.com>
2017-08-22 10:42:04 +0200

    user factory generating en

--- resources/views/templates/user/factory.blade.php
+++ resources/views/templates/user/factory.blade.php
@@ -2,9 +2,9 @@
 $factory->define({{ $modelFullName }}::class, function (Faker\Generator $faker) {
     return [
         @foreach($columns as $col)@if($col['name'] == 'activated')'{{ $col['name'] }}' => true,
+@elseif($col['name'] == 'language')'{{ $col['name'] }}' => 'en',
         @else'{{ $col['name'] }}' => {!! $col['faker'] !!},
-        @endif
-        @endforeach
+@endif        @endforeach
 
     ];
 });
