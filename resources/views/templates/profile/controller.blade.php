f79aee5e309b738c64867dcba09168ff540b2b0a
David Běhal <github@dejw.sk>
2017-11-20 15:44:07 +0100

    Fixed missing use Rule in profil econtroller (#8)

--- resources/views/templates/profile/controller.blade.php
+++ resources/views/templates/profile/controller.blade.php
@@ -4,6 +4,7 @@
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rule;
 
 class ProfileController extends Controller
 {
