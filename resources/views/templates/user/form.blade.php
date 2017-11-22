081b6a08e211cfd5e877e286e686916eb07e6018
Matej Minar <matej.minar@triad.sk>
2017-11-22 17:08:40 +0100

    responsive markup

--- resources/views/templates/user/form.blade.php
+++ resources/views/templates/user/form.blade.php
@@ -1,16 +1,18 @@
 @if($hasTranslatable)<div class="row form-inline" style="padding-bottom: 10px;" v-cloak>
-    <div :class="{'col-xl-10 col-md-11 text-right': !isFormLocalized, 'col text-center': isFormLocalized }">
+    <div :class="{'col-xl-10 col-md-11 text-right': !isFormLocalized, 'col text-center': isFormLocalized, 'hidden': onSmallScreen }">
         <small>@{{ trans('brackets/admin-ui::admin.forms.currently_editing_translation') }}<span v-if="!isFormLocalized && otherLocales.length > 1"> @{{ trans('brackets/admin-ui::admin.forms.more_can_be_managed') }}</span><span v-if="!isFormLocalized"> | <a href="#" @click.prevent="showLocalization">@{{ trans('brackets/admin-ui::admin.forms.manage_translations') }}</a></span></small>
         <i class="localization-error" v-if="!isFormLocalized && showLocalizedValidationError"></i>
     </div>
-    <div class="col text-center" v-if="isFormLocalized" v-cloak>
+
+    <div class="col text-center" :class="{'language-mobile': onSmallScreen, 'has-error': !isFormLocalized && showLocalizedValidationError}" v-if="isFormLocalized || onSmallScreen" v-cloak>
         <small>@{{ trans('brackets/admin-ui::admin.forms.choose_translation_to_edit') }}
             <select class="form-control" v-model="currentLocale">
-                <option v-for="locale in otherLocales" :value="locale">@@{{ locale.toUpperCase() }}</option>
+                <option :value="defaultLocale" v-if="onSmallScreen">@{{ defaultLocale.toUpperCase() }}</option>
+                <option v-for="locale in otherLocales" :value="locale">@{{ locale.toUpperCase() }}</option>
             </select>
             <i class="localization-error" v-if="isFormLocalized && showLocalizedValidationError"></i>
-            |
-            <a href="#" @click.prevent="hideLocalization">@{{ trans('brackets/admin-ui::admin.forms.hide') }}</a>
+            <span>|</span>
+            <a href="#" @click.prevent="hideLocalization">{{ trans('brackets/admin-ui::admin.forms.hide') }}</a>
         </small>
     </div>
 </div>
@@ -81,7 +83,7 @@
 </div>
 @elseif($col['type'] == 'json')<div class="row">
     @@foreach($locales as $locale)
-        <div class="col"@@if(!$loop->first) v-show="isFormLocalized && currentLocale == '@{{ $locale }}'" v-cloak @@endif>
+        <div class="col-md" v-show="shouldShowLangGroup('@{{ $locale }}')" v-cloak>
             <div class="form-group row align-items-center" :class="{'has-danger': errors.has('{{ $col['name'] }}_@{{ $locale }}'), 'has-success': this.fields.{{ $col['name'] }}_@{{ $locale }} && this.fields.{{ $col['name'] }}_@{{ $locale }}.valid }">
                 <label for="{{ $col['name'] }}_@{{ $locale }}" class="col-md-2 col-form-label text-md-right">{{'{{'}} trans('admin.{{ $modelLangFormat }}.columns.{{ $col['name'] }}') }}</label>
                 <div class="col-md-9" :class="{'col-xl-8': !isFormLocalized }">
