59c34e7d584aa811789794d4703d4efd0114d16b
drzik <drzik@eazy.sk>
2018-12-10 17:11:35 +0100

    removed key without id

--- src/Services/FormatTranslatableStringService.php
+++ src/Services/FormatTranslatableStringService.php
@@ -4,26 +4,6 @@ namespace Brackets\AdminGenerator\Services;
 
 class FormatTranslatableStringService
 {
-
-    /**
-     * Format string as key and remove 'id' if it's in the end.
-     *
-     * @param $string
-     * @return string
-     */
-    public function keyWithoutId($string)
-    {
-        $pieces = explode('_', $string);
-        $key = str_replace('_', ' ', $string);
-
-        if (strtolower(array_pop($pieces)) === 'id') {
-            $key = preg_replace('/\W\w+\s*(\W*)$/', '$1', $key);
-            return str_replace(' ', '_', $key);
-        } else {
-            return $string;
-        }
-    }
-
     /**
      * Format string as value and remove 'id' if it's in the end.
      *
