e4b422ff79773118264755d2880a3c6fa1985613
David Běhal <behal.david@gmail.com>
2019-01-04 16:05:12 +0100

    Fixed hardcoded name

--- resources/views/templates/profile/controller.blade.php
+++ resources/views/templates/profile/controller.blade.php
@@ -8,7 +8,7 @@ use Illuminate\Validation\Rule;
 
 class ProfileController extends Controller
 {
-    public $adminUser;
+    public ${{ $modelVariableName }};
 
     /**
      * Guard used for admin user
