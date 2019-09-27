ac6f520288605705e4426ad0b8d70f654d779a5e
Richard <riso.dominik@gmail.com>
2019-09-27 16:09:12 +0200

    refactoring routes

--- resources/views/routes.blade.php
+++ resources/views/routes.blade.php
@@ -1,16 +1,20 @@
 
 /* Auto-generated admin routes */
 Route::middleware(['auth:' . config('admin-auth.defaults.guard'), 'admin'])->group(static function () {
-    {!! str_pad("Route::get('/admin/".$resource."',", 60) !!}'{{ $controllerPartiallyFullName }}@index');
-    {!! str_pad("Route::get('/admin/".$resource."/create',", 60) !!}'{{ $controllerPartiallyFullName }}@create');
-    {!! str_pad("Route::post('/admin/".$resource."',", 60) !!}'{{ $controllerPartiallyFullName }}@store');
-    {!! str_pad("Route::get('/admin/".$resource."/{".$modelVariableName."}/edit',", 60) !!}'{{ $controllerPartiallyFullName }}@edit')->name('admin/{{ $resource }}/edit');
+    Route::prefix('admin')->namespace('Admin')->name('admin/')->group(static function() {
+        Route::prefix('{{ $resource }}')->name('{{ $resource }}/')->group(static function() {
+            {!! str_pad("Route::get('/',", 60) !!}'{{ $controllerPartiallyFullName }}@index')->name('index');
+            {!! str_pad("Route::get('/create',", 60) !!}'{{ $controllerPartiallyFullName }}@create')->name('create');
+            {!! str_pad("Route::post('/',", 60) !!}'{{ $controllerPartiallyFullName }}@store')->name('store');
+            {!! str_pad("Route::get('/{".$modelVariableName."}/edit',", 60) !!}'{{ $controllerPartiallyFullName }}@edit')->name('edit');
 @if(!$withoutBulk)
-    {!! str_pad("Route::post('/admin/".$resource."/bulk-destroy',", 60) !!}'{{ $controllerPartiallyFullName }}@bulkDestroy')->name('admin/{{ $resource }}/bulk-destroy');
+            {!! str_pad("Route::post('/bulk-destroy',", 60) !!}'{{ $controllerPartiallyFullName }}@bulkDestroy')->name('bulk-destroy');
 @endif
-    {!! str_pad("Route::post('/admin/".$resource."/{".$modelVariableName."}',", 60) !!}'{{ $controllerPartiallyFullName }}@update')->name('admin/{{ $resource }}/update');
-    {!! str_pad("Route::delete('/admin/".$resource."/{".$modelVariableName."}',", 60) !!}'{{ $controllerPartiallyFullName }}@destroy')->name('admin/{{ $resource }}/destroy');
+            {!! str_pad("Route::post('/{".$modelVariableName."}',", 60) !!}'{{ $controllerPartiallyFullName }}@update')->name('update');
+            {!! str_pad("Route::delete('/{".$modelVariableName."}',", 60) !!}'{{ $controllerPartiallyFullName }}@destroy')->name('destroy');
 @if($export)
-    {!! str_pad("Route::get('/admin/".$resource."/export',", 60) !!}'{{ $controllerPartiallyFullName }}@export')->name('admin/{{ $resource }}/export');
+            {!! str_pad("Route::get('/export',", 60) !!}'{{ $controllerPartiallyFullName }}@export')->name('export');
 @endif
+        });
+    });
 });
